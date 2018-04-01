2a12f051c310e1e46a7f93cacfaf898a337d172c
Nicolas Page <NicolasPage@MacBook-Pro-de-Nicolas.local>
2018-04-01 19:51:25 -0400

    modification gestionnaire

--- TP5/GestionnaireUsagers.h
+++ TP5/GestionnaireUsagers.h
@@ -27,3 +27,35 @@ Les m�thodes retrouv�es de la classe Gestionnaire sont :
 - void reinitialiser();
 - void afficherProfils() const;
 */
+//
+
+/********************************************
+ * Titre: Travail pratique #5 - GestionnaireUsagers.h
+ * Date: 9 mars 2018
+ * Auteur: Ryan Hardie
+ *******************************************/
+
+#pragma once
+
+#include "GestionnaireGenerique.h"
+#include <set>
+#include "Foncteur.h"
+// TODO : CrÈer la classe GestionnaireUsager
+
+// TODO : La classe ressemble beaucoup ‡ la classe Gestionnaire
+/*
+ Les mÈthodes retrouvÈes de la classe Gestionnaire sont :
+ - double obtenirChiffreAffaires() const;
+ - void encherir(Client *client, ProduitAuxEncheres *produit, double montant) const;
+ - void reinitialiser();
+ - void afficherProfils() const;
+ */
+class GestionnaireUsagers: public GestionnaireGenerique<Usager, set<Usager*>, FoncteurAjouterUsager, FoncteurSupprimerUsager>
+{
+public:
+    double obtenirChiffreAffaires() const;
+    void encherir(Client *client, ProduitAuxEncheres *produit, double montant) const;
+    void reinitialiser();
+    void afficherLesProfils() const;
+};
+
