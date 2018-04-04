d2646135a1c8c6fefe6dc016c3d6a3f83cb60d00
JeanSavary <jean.savary@imt-atlantique.net>
2018-04-04 09:44:13 -0400

    errors

--- TP5/GestionnaireProduits.cpp
+++
@@ -1,7 +0,0 @@
-/********************************************
-* Titre: Travail pratique #5 - GestionnaireProduits.cpp
-* Date: 9 mars 2018
-* Auteur: Ryan Hardie
-*******************************************/
-
-#include "GestionnaireProduits.h"
